c91ae269c065c4024f6a2987607103e3cc7a13ef
Eunji Kim <chgchs10@naver.com>
2024-10-10 16:25:29 +0900

    post

--- calm_lake_project/lib/model/post.dart
+++ calm_lake_project/lib/model/post.dart
@@ -14,4 +14,22 @@ class Post extends GetxController {
     required this.contents,
     required this.userId,
   });
+  /*
+        p.seq, //0
+        p.post_user_id, //1
+        p.date, //2
+        p.image, //3
+        p.contents, //4
+        p.public, //5
+        p.post_nickname, //6
+        f.seq AS favorite_seq, //7
+        f.user_id AS favorite_user_id, //8
+        f.post_seq AS favorite_post_seq, //9
+        f.favorite,//10
+        h.seq AS hate_seq,//11 
+        h.user_id AS hate_user_id,//12 
+        h.post_seq AS hate_post_seq, //13
+        h.hate,//14
+        count(c.seq) as comment_count //15
+*/
 }
