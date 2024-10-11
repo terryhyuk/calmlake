0e1937d49c01a01857dd4cf8a812bf1b99fb43c3
LrZl <dingtti@naver.com>
2024-10-11 16:30:00 +0900

    addfriends
    chat

--- calm_lake_project/lib/model/message.dart
+++ calm_lake_project/lib/model/message.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Message {
   String id;
+  String roomId;
+  String roomName;
   String userID;
   String nickname;
   String contents;
@@ -9,6 +11,8 @@ class Message {
 
   Message({
     required this.id,
+    required this.roomId,
+    required this.roomName,
     required this.userID,
     required this.nickname,
     required this.contents,
@@ -18,6 +22,8 @@ class Message {
   factory Message.fromMap(Map<String, dynamic> map, String id) {
     return Message(
       id: id,
+      roomId: map['roomId']??'',
+      roomName: map['roomName']?? '',
       userID: map['userID'] ?? '',
       nickname: map['nickname'] ?? '',
       contents: map['contents'] ?? '',
@@ -28,6 +34,8 @@ class Message {
   Map<String, dynamic> toMap() {
     return {
       'userID': userID,
+      'roomId' : roomId,
+      'roomName': roomName,
       'nickname': nickname,
       'contents': contents,
       'timestamp': Timestamp.fromDate(timestamp),
--- calm_lake_project/lib/vm/serchfriends_controller.dart
+++ calm_lake_project/lib/vm/serchfriends_controller.dart
@@ -1,13 +1,12 @@
 import 'dart:convert';
 
-import 'package:calm_lake_project/model/addfriends.dart';
 import 'package:get/state_manager.dart';
 import 'package:http/http.dart' as http;
 
 class SearchFriendsController extends GetxController {
 
   List friends = [].obs;
-  List addfriend = [].obs;
+  
 
   searchFriendsJSON(String searchText) async {
     var url =
@@ -19,20 +18,4 @@ class SearchFriendsController extends GetxController {
     friends.addAll(results);
   }
 
-  // 친구추가
-  addfriendsJSONData(Addfriends addfriends)async{
-    var url = Uri.parse('http://127.0.0.1:8000/friends/insertfriends')
-                    .replace(queryParameters: {
-      'user_id': addfriends.user_id,
-      'accept': addfriends.accept,
-      'addid': addfriends.addid,
-      'date': addfriends.date
-    });
-    
-    var response = await http.get(url);
-    addfriend.clear();
-    var dataConvertedJSON = json.decode(utf8.decode(response.bodyBytes));
-    List results = dataConvertedJSON['results'];
-    addfriend.addAll(results);
-  }
 }
