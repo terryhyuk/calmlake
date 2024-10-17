d845d2a3b31ee43479099776dc61876fdf4dca04
LrZl <dingtti@naver.com>
2024-10-17 17:00:59 +0900

    signup
    textfield

--- calm_lake_project/lib/view/login/sign_up.dart
+++ calm_lake_project/lib/view/login/sign_up.dart
@@ -42,132 +42,136 @@ class SignUp extends StatelessWidget {
             fontWeight: FontWeight.bold
           ),),
         ),
-        body: GetBuilder<LoginHandler>(
-          builder: (controller) {
-            return Padding(
-              padding: const EdgeInsets.fromLTRB(20,0,20,0),
-              child: Center(
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Column(
-                          children: [
-                            SizedBox(
-                              width: 230,
-                              child: TextField(
-                                controller: idController,
-                                decoration: const InputDecoration(labelText: 'ID를 입력하세요.'),
+        body: SingleChildScrollView(
+          child: GetBuilder<LoginHandler>(
+            builder: (controller) {
+              return Padding(
+                padding: const EdgeInsets.fromLTRB(20,0,20,0),
+                child: Center(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Column(
+                            children: [
+                              SizedBox(
+                                width: 230,
+                                child: TextField(
+                                  controller: idController,
+                                  decoration: const InputDecoration(labelText: 'ID를 입력하세요.'),
+                                ),
                               ),
-                            ),
-                            Text(controller.idLabel.toString(),
-                            style: TextStyle(
-                              color: controller.idLabel.toString()=='사용할 수 없는 ID입니다.' ? Colors.red : Colors.green
-                            ),)
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(15,0,0,0),
-                          child: ElevatedButton(
-                            onPressed: () async{
-                              await controller.idDoublecheck(idController.text.trim());
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: const Color(0xFFF9D3CC),
-                              foregroundColor: Colors.black
-                            ), 
-                            child: const Text('중복검사'),
-                            ),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0,10,0,10),
-                      child: TextField(
-                        controller: pwController,
-                        decoration: InputDecoration(labelText: pwController.text=='' ? 'Password를 입력하세요.' : controller.checkResult, labelStyle: TextStyle(
-                          color: controller.pwColor
-                        )),
-                        onChanged: (value) {
-                          controller.validatePassword(pwController.text.trim());
-                        },
-                      ),
-                    ),
-                    TextField(
-                      controller: pwcheckController,
-                      decoration: InputDecoration(labelText: pwcheckController.text=='' ? 'Password를 다시 입력하세요.':controller.doublePw, 
-                      labelStyle: TextStyle(
-                        color: controller.doublePwColor
-                      )),
-                      onChanged: (value) {
-                        controller.doublecheckPw(pwController.text.trim(), pwcheckController.text.trim());
-                      },
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0,10,0,10),
-                      child: TextField(
-                        controller: emailController,
-                        decoration: InputDecoration(labelText: emailController.text=='' ? 'Email을 입력하세요.' : controller.checkEmail,
-                        labelStyle: TextStyle(
-                          color: controller.emailColor
-                        )),
-                        onChanged: (value) {
-                          controller.validateEmail(emailController.text.trim());
-                        },
-                      ),
-                    ),
-                    Column(
-                      children: [
-                        TextField(
-                          controller: nickNameController,
-                          decoration:
-                              const InputDecoration(labelText: 'Nickname을 입력하세요.'),
-                              onChanged: (value) async {
-                                await controller.nickDoublecheck(nickNameController.text.trim());
+                              Text(controller.idLabel.toString(),
+                              style: TextStyle(
+                                color: controller.idLabel.toString()=='사용할 수 없는 ID입니다.' ? Colors.red : Colors.green
+                              ),)
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(15,0,0,0),
+                            child: ElevatedButton(
+                              onPressed: () async{
+                                await controller.idDoublecheck(idController.text.trim());
                               },
-                        ),
-                        Text(controller.nickLabel,
-                        style: TextStyle(
-                          color: controller.nickLabel=='사용할 수 없는 Nickname입니다.' ? Colors.red : Colors.green
-                        ),
-                        ),
-                      ],
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.fromLTRB(0,30,0,5),
-                      child: Text('다음은 비밀번호 재설정에 사용될 질문입니다.',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xFFF9D3CC),
+                                foregroundColor: Colors.black
+                              ), 
+                              child: const Text('중복검사'),
+                              ),
+                          ),
+                        ],
                       ),
-                      ),
-                    ),
-                    TextField(
-                      controller: pwAnswerController,
-                      decoration:
-                          const InputDecoration(labelText: '가장 기억에 남는 스승의 이름은 무엇입니까?'),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0,70,0,0),
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFFD7EFC9),
-                          foregroundColor: Colors.black
-                        ),
-                          onPressed: () {
-                            insertAction();
-                            controller.idLabel='';
-                            controller.nickLabel='';
-                            controller.pwColor=Colors.black;
-                            controller.doublePwColor=Colors.black;
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0,10,0,10),
+                        child: TextField(
+                          controller: pwController,
+                          obscureText: true,
+                          decoration: InputDecoration(labelText: pwController.text=='' ? 'Password를 입력하세요.' : controller.checkResult, labelStyle: TextStyle(
+                            color: controller.pwColor
+                          )),
+                          onChanged: (value) {
+                            controller.validatePassword(pwController.text.trim());
                           },
-                          child: const Text('Sign Up')),
-                    ),
-                  ],
+                        ),
+                      ),
+                      TextField(
+                        controller: pwcheckController,
+                        obscureText: true,
+                        decoration: InputDecoration(labelText: pwcheckController.text=='' ? 'Password를 다시 입력하세요.':controller.doublePw, 
+                        labelStyle: TextStyle(
+                          color: controller.doublePwColor
+                        )),
+                        onChanged: (value) {
+                          controller.doublecheckPw(pwController.text.trim(), pwcheckController.text.trim());
+                        },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0,10,0,10),
+                        child: TextField(
+                          controller: emailController,
+                          decoration: InputDecoration(labelText: emailController.text=='' ? 'Email을 입력하세요.' : controller.checkEmail,
+                          labelStyle: TextStyle(
+                            color: controller.emailColor
+                          )),
+                          onChanged: (value) {
+                            controller.validateEmail(emailController.text.trim());
+                          },
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          TextField(
+                            controller: nickNameController,
+                            decoration:
+                                const InputDecoration(labelText: 'Nickname을 입력하세요.'),
+                                onChanged: (value) async {
+                                  await controller.nickDoublecheck(nickNameController.text.trim());
+                                },
+                          ),
+                          Text(controller.nickLabel,
+                          style: TextStyle(
+                            color: controller.nickLabel=='사용할 수 없는 Nickname입니다.' ? Colors.red : Colors.green
+                          ),
+                          ),
+                        ],
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(0,30,0,5),
+                        child: Text('다음은 비밀번호 재설정에 사용될 질문입니다.',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18
+                        ),
+                        ),
+                      ),
+                      TextField(
+                        controller: pwAnswerController,
+                        decoration:
+                            const InputDecoration(labelText: '가장 기억에 남는 스승의 이름은 무엇입니까?'),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0,70,0,0),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFFD7EFC9),
+                            foregroundColor: Colors.black
+                          ),
+                            onPressed: () {
+                              insertAction();
+                              controller.idLabel='';
+                              controller.nickLabel='';
+                              controller.pwColor=Colors.black;
+                              controller.doublePwColor=Colors.black;
+                            },
+                            child: const Text('Sign Up')),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
       ),
     );
